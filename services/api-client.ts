409c83c9352172d330275bec8f81ea8ea46e945a
ShehanSuraweera <shehanssuraweera@gmail.com>
2025-01-27 12:41:28 +0530

    link to azure

--- services/api-client.ts
+++ services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://16.171.138.132:4001",
+  baseURL: "https://ceyloncars.azurewebsites.net",
   withCredentials: true,
 });
