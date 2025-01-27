238608ad1f0a4078958ba627e4aff1b1a44d54af
ShehanSuraweera <shehanssuraweera@gmail.com>
2025-01-27 13:58:34 +0530

    update api-client

--- services/api-client.ts
+++ services/api-client.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 
+// Check if we're in the browser or on the server
+const isLocal =
+  typeof window !== "undefined" && window.location.hostname === "localhost";
+const baseURL = isLocal
+  ? "http://localhost:4001"
+  : "https://ceyloncars.azurewebsites.net";
+
 export default axios.create({
-  baseURL: "https://ceyloncars.azurewebsites.net",
+  baseURL: baseURL,
   withCredentials: true,
 });
